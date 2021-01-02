a701fff9a5a6914ca5d148ab72c35b85e30e7b39
Andrey Zak <amzak.only@gmail.com>
2021-01-03 01:28:45 +0300

    First draft of set z offset command

--- src/MigoToolCli/Program.cs
+++ src/MigoToolCli/Program.cs
@@ -3,6 +3,8 @@ using System.CommandLine;
 using System.CommandLine.Invocation;
 using System.CommandLine.Parsing;
 using System.Threading.Tasks;
+using MigoLib;
+using Command = System.CommandLine.Command;
 
 namespace MigoToolCli
 {
@@ -24,13 +26,23 @@ namespace MigoToolCli
             return await root.InvokeAsync(args).ConfigureAwait(false);
         }
 
-        private static void SetZOffset(ParseResult result, double zOffset)
+        private static async Task SetZOffset(ParseResult result, double zOffset)
         {
             var endpoint = result.RootCommandResult
                 .OptionResult("--endpoint")?
                 .GetValueOrDefault<MigoEndpoint>();
+
+            if (endpoint == default)
+            {
+                throw new ArgumentException("Invalid endpoint");
+            }
+
+            var migo = new Migo(endpoint.Ip.ToString(), endpoint.Port);
+            var resultOffset = await migo.SetZOffset(zOffset)
+                .ConfigureAwait(false);
             
-            Console.WriteLine(zOffset.ToString(), endpoint);
+            var success = resultOffset.Equals(zOffset);
+            Console.WriteLine(success ? "Ok" : "Fail");
         }
     }
 }
