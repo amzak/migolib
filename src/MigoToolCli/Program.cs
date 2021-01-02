190d33c23e5b31bed50d3741d94c3c961d7528cb
Andrey Zak <amzak.only@gmail.com>
2021-01-02 20:43:18 +0300

    Initial cli tool implementation

---
+++ src/MigoToolCli/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Threading.Tasks;
+
+namespace MigoToolCli
+{
+    class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            var zOffsetCommand = new Command("zoffset", "Sets zoffset");
+            zOffsetCommand.AddArgument(new Argument<double>("zoffset"));
+            zOffsetCommand.Handler = CommandHandler.Create<double>(SetZOffset);
+
+            var setCommand = new Command("set", "Sets parameter");
+            setCommand.AddCommand(zOffsetCommand);
+
+            var root = new RootCommand("Unofficial Migo CLI tool");
+            root.AddCommand(setCommand);            
+
+            return await root.InvokeAsync(args).ConfigureAwait(false);
+        }
+        
+        private static void SetZOffset(double zOffset)
+        {
+            Console.WriteLine(zOffset);
+        }
+    }
+}
